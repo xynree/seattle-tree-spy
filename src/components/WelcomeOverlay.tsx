f48545ab504a8bc3537955ed04f1cf5138620705
Xinrui Chen <xynree@gmail.com>
2026-01-24 11:26:58 -0800

    allow overlay to close bg

--- src/components/WelcomeOverlay.tsx
+++ src/components/WelcomeOverlay.tsx
@@ -6,7 +6,10 @@ export default function WelcomeOverlay() {
   return closed ? (
     <>:</>
   ) : (
-    <div className="absolute top-0 w-screen h-screen bg-black/20 z-10">
+    <div
+      onClick={() => setClosed(true)}
+      className="absolute top-0 w-screen h-screen bg-black/20 z-10"
+    >
       <div className="max-w-md flex justify-between bg-white rounded-3xl z-10 absolute left-2/5 top-1/3 p-6 text-center shadow-lg">
         <div className="flex flex-col gap-4">
           <h2 className="text-lg font-bold">Exploring Seattle's Trees</h2>
